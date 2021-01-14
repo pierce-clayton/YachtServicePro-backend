6b73708eccf1b2ed19cfc62b7348eb8d2baa311b
Clayton Pierce <46853031+technicholy@users.noreply.github.com>
2021-01-14 15:32:44 -0800

    updates session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,5 @@
 Rails.application.config.session_store :cookie_store, key: '_ysp_app',
                                                       secure: Rails.env.production?,
+                                                      same_site: :lax,
                                                       domain: :all
 
