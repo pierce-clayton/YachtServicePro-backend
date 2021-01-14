59612023773a7163576868df640ccd53ea42425d
Clayton Pierce <46853031+technicholy@users.noreply.github.com>
2021-01-14 15:04:40 -0800

    updates session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,2 @@
-if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_ysp_app', domain: :all
-else
-  Rails.application.config.session_store :cookie_store, key: '_ysp_app'
-end
+Rails.application.config.session_store :cookie_store, key: '_ysp_app', domain: :all
+
