b27310cb68a30b72768a2a8f26f3cefeb118b22c
Clayton Pierce <46853031+technicholy@users.noreply.github.com>
2021-01-14 14:27:49 -0800

    corrects cookie domain again

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_auth_app', domain: 'https://gentle-caverns-38062.herokuapp.com/'
+  Rails.application.config.session_store :cookie_store, key: '_auth_app', domain: 'https://ysp.baracus.rocks/'
 else
   Rails.application.config.session_store :cookie_store, key: '_auth_app'
 end
