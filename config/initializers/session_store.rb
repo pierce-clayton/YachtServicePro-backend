b486fd1dff33f36812465feb893fcd337934fd95
Clayton Pierce <46853031+technicholy@users.noreply.github.com>
2021-01-14 14:47:33 -0800

    changes cookie store to fix persistence

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: '_auth_app', domain: 'https://ysp.baracus.rocks/'
+  Rails.application.config.session_store :cookie_store, key: '_ysp_app', domain: :all
 else
-  Rails.application.config.session_store :cookie_store, key: '_auth_app'
+  Rails.application.config.session_store :cookie_store, key: '_ysp_app'
 end
