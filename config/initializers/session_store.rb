3f4fd1ff0fb0abe4ad9fc2ba110044397c523589
Clayton Pierce <46853031+technicholy@users.noreply.github.com>
2021-01-14 16:41:10 -0800

    disables http only cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,6 @@
 Rails.application.config.session_store :cookie_store, key: '_ysp_app',
                                                       secure: true,
                                                       same_site: :none,
+                                                      httponly: false,
                                                       domain: :all
 
