5113ae754e96af8d76c24e91fc459a8c668d709e
Clayton Pierce <46853031+technicholy@users.noreply.github.com>
2021-01-18 16:00:42 -0800

    tries to correct marina display

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Marina.find_or_create_by!(name: 'Oak Harbor', address_line_1: '1401 SE Catalina Dr', address_line_2: '', city: 'Oak Harbor', state:'WA', zip: 98277, description: 'city marina')
-Marina.find_or_create_by!(name: 'Anchor Cove Marina', address_line_1: '1600 5th St', address_line_2: '', city: 'Anacortes', state:'WA', zip: 98221, description: 'city marina')
-Marina.find_or_create_by!(name: 'Fidalgo Marina', address_line_1: '3101 V Pl', address_line_2: '', city: 'Anacortes', state:'WA', zip: 98221, description: 'city marina')
-Marina.find_or_create_by!(name: 'Skyline Marine Center', address_line_1: '2011 Skyline Way', address_line_2: '', city: 'Anacortes', state:'WA', zip: 98221, description: 'city marina')
-Marina.find_or_create_by!(name: 'Port of South Whidbey Harbor at Langley', address_line_1: '228 Wharf St', address_line_2: '', city: 'Langley', state:'WA', zip: 98260, description: 'city marina')
+Marina.create!(name: 'Oak Harbor', address_line_1: '1401 SE Catalina Dr', address_line_2: '', city: 'Oak Harbor', state:'WA', zip: 98277, description: 'city marina')
+Marina.create!(name: 'Anchor Cove Marina', address_line_1: '1600 5th St', address_line_2: '', city: 'Anacortes', state:'WA', zip: 98221, description: 'city marina')
+Marina.create!(name: 'Fidalgo Marina', address_line_1: '3101 V Pl', address_line_2: '', city: 'Anacortes', state:'WA', zip: 98221, description: 'city marina')
+Marina.create!(name: 'Skyline Marine Center', address_line_1: '2011 Skyline Way', address_line_2: '', city: 'Anacortes', state:'WA', zip: 98221, description: 'city marina')
+Marina.create!(name: 'Port of South Whidbey Harbor at Langley', address_line_1: '228 Wharf St', address_line_2: '', city: 'Langley', state:'WA', zip: 98260, description: 'city marina')
