02c75f38ddcf9ef8cf5540ac2a12a865a933913f
Clayton Pierce <46853031+technicholy@users.noreply.github.com>
2021-01-21 10:01:39 -0800

    one time for LA

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,16 @@
 class RegistrationsController < ApplicationController
+  before_action :set_stripe_key
   def create
     if params[:user]
       user = User.create!(email: params[:user][:email],
                           password: params[:user][:password], 
                           password_confirmation: params[:user][:password_confirmation])
     else
+       stripe_customer = Stripe::Customer.create(email: params[:customer][:email])
        customer = Customer.create!(email: params[:customer][:email],
                                    password: params[:customer][:password],
-                                   password_confirmation: params[:customer][:password_confirmation])
+                                   password_confirmation: params[:customer][:password_confirmation],
+                                   stripe_id: stripe_customer.id)
     end
 
     if user
@@ -25,5 +28,11 @@ class RegistrationsController < ApplicationController
     else
       render json: { status: 422 }
     end
+  end
+
+  private
+
+  def set_stripe_key
+    Stripe.api_key = 'sk_test_51I8VgBLQ8SRg06CGQYgJKACHjoPnXoN0pcNzoG3aCt4dXoDVJsMPL6HiYw0ISS8HGnx6hr3NGjbntXF5QTbFdqPW00idukCUXR'
   end
 end
