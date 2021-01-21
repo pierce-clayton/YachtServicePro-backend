18878de4578a505d43ae737805de22640b897f3b
Clayton Pierce <46853031+technicholy@users.noreply.github.com>
2021-01-21 10:27:49 -0800

    fixes cookie warning message

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 Rails.application.config.session_store :cookie_store, key: '_ysp_app',
                                                       secure: true,
-                                                      same_site: :none,
+                                                      same_site: 'None',
                                                       httponly: false,
                                                       domain: :all
 
