ef496b951c5c9c0f942cf8f6c7cbd691d0b2ed53
Clayton Pierce <46853031+technicholy@users.noreply.github.com>
2021-01-25 12:00:09 -0800

    updates yacht save

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -1,6 +1,6 @@
 class Yacht < ApplicationRecord
   belongs_to :marina
 
-  has_many :customer_yachts
+  has_many :customer_yachts, dependent: :destroy
   has_many :customers, through: :customer_yachts
 end
